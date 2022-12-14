a785ec26ecedb8c10ecd847d974c6234c3360ce6
Stanrley Silva <stanrley.silva@tegrus.com.br>
2022-12-14 20:57:15 -0300

    [Feat] Criando schema, gerando migration, método create básico no service que cria uma notificação no banco de dados

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,34 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService, User } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
 
-@Controller('app')
+@Controller('notifications')
 export class AppController {
-  // Inversão de dependência
-  // Esta classe recebe (ao invés de buscar) uma variável do tipo "AppService"
-  // (que é uma dependência).
-  // - DÚVIDA: O nest se encarrega de instanciar este controller e passar o
-  //   appService por baixo dos panos?
-  // - RESPOSTA: O Nest goza também a Injeção de dependência, que basicamente
-  // automatiza a injeção do "appService" no hora em qua o "AppController" é
-  // instanciado.
-  // A injeção de dependência funciona porque lá no "app.module.ts" é providenciado
-  // uma classe do tipo "AppService", fazendo com que o Nest consiga identificar
-  // a dependência, e para injetar, a classe AppService deve possuir o decorator
-  // @Injectable()!!!
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-  @Get('hello-route')
-  getHello(): string {
-    return this.appService.getHello();
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
   }
 
-  @Get('rota-ola-mundo')
-  getTest(): string {
-    return 'Olá mundo!';
-  }
-
-  @Get('user')
-  getUser(): User {
-    return this.appService.getUser();
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Fernando curtiu seu comentãrio.',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
+
+// Inversão de dependência
+// Esta classe recebe (ao invés de buscar) uma variável do tipo "AppService"
+// (que é uma dependência).
+// - DÚVIDA: O nest se encarrega de instanciar este controller e passar o
+//   appService por baixo dos panos?
+// - RESPOSTA: O Nest goza também a Injeção de dependência, que basicamente
+// automatiza a injeção do "appService" no hora em qua o "AppController" é
+// instanciado.
+// A injeção de dependência funciona porque lá no "app.module.ts" é providenciado
+// uma classe do tipo "AppService", fazendo com que o Nest consiga identificar
+// a dependência, e para injetar, a classe AppService deve possuir o decorator
+// @Injectable()!!!
